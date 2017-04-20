4256f5f9291f67578916d8dcb075be2c9abc8aa4
hpstream <915414300@qq.com>
2017-04-20 09:57:26 +0800

    //引入自动化构建工具，gulp可以对代码进行压缩，合并等操作，含有热加载的功能

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require("gulp");
+const $    = require("gulp-load-plugins")();
+const browserSync = require('browser-sync').create();
+const reload      = browserSync.reload;
+gulp.task("minjs",function(){
+        gulp.src("./js/*.js")
+            .pipe($.concat('main.js'))
+            .pipe(gulp.dest("./dest/js"))
+            .pipe($.rename({suffix: '.min'}))
+            .pipe($.uglify())
+            .pipe(gulp.dest('./dest/js'));
+    });
+gulp.task("mincss",function(){
+        gulp.src("./css/*.css")
+            .pipe($.concat('main.css'))
+            .pipe(gulp.dest("./dest/css"))
+            .pipe($.rename({suffix: '.min'}))
+            .pipe($.minifyCss())
+            .pipe(gulp.dest('./dest/css'));
+    })
+
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("./js/*.js").on('change', reload);
+    gulp.watch("./css/*.css").on('change', reload);
+    gulp.watch("*.html").on('change', reload);
+});
